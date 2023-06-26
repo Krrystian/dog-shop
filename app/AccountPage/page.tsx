350a905d481eaccc5a654a65d559818bcb715d0e
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-06-26 13:16:42 +0200

    Dashboard

--- app/AccountPage/page.tsx
+++ app/AccountPage/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 import getCurrentUser from "../actions/getCurrentUser";
+import AccountPage from "./components/AccountPage";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "300" });
+
+function AdminDashboard() {
+  return <div>a</div>;
+}
+
+function AccountDashboard(userName: String | null) {
+  return <AccountPage name={userName} />;
+}
 
 export default async function page() {
   const currentUser = await getCurrentUser();
-  return <div className="text-9xl text-white">Welcome {currentUser?.name}</div>;
+  return (
+    <div className={`${poppins.className} h-screen w-screen overflow-hidden`}>
+      {currentUser?.role === "user"
+        ? AccountDashboard(currentUser.name)
+        : AdminDashboard()}
+    </div>
+  );
 }
