5bfb5f283150afc51f980eebb71dd725f3787067
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-08-18 02:12:21 +0200

    UI/UX of orders

--- app/AccountPage/components/AccountOrders.tsx
+++ app/AccountPage/components/AccountOrders.tsx
@@ -1,9 +1,27 @@
 "use client";
 import useAccountMenu from "@/app/hooks/useAccountMenu";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import OrderPosition from "./OrderPosition";
 
-const AccountOrders = () => {
+interface AccountOrdersProps {
+  userId: string;
+}
+const AccountOrders: React.FC<AccountOrdersProps> = ({ userId }) => {
   const useAccount = useAccountMenu();
+  const [current, setCurrent] = useState<number>(0);
+  const [orderList, setOrderList] = useState<any>();
+  const [max, setMax] = useState<number>(1);
+  useEffect(() => {
+    axios
+      .get(`/api/order/${current}/${userId}`)
+      .then((response) => {
+        setOrderList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [current]);
   return (
     <div
       className={
@@ -12,7 +30,19 @@ const AccountOrders = () => {
           : "hidden z-[-1]"
       }
     >
-      <h2 className="text-3xl">My orders</h2>
+      <h2 className="text-3xl text-center">My orders</h2>
+      <div className="flex flex-col-reverse gap-2 ">
+        {/* BAZA DANYCH SORTOWANIE */}
+        {orderList &&
+          orderList.map((order: any) => (
+            <OrderPosition
+              id={order.id}
+              orderTime={order.createdAt}
+              products={order.orderDetail}
+              key={order.id}
+            />
+          ))}
+      </div>
     </div>
   );
 };
