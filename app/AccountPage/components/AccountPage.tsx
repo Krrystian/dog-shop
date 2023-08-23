62e265fd39821b858b8bc179903cb4324043bb92
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-08-23 02:22:22 +0200

    Margin fixes

--- app/AccountPage/components/AccountPage.tsx
+++ app/AccountPage/components/AccountPage.tsx
@@ -8,10 +8,10 @@ interface AccountPageProps {
 const AccountPage: React.FC<AccountPageProps> = ({ name, children }) => {
   return (
     <div className="h-screen w-screen bg-zinc-200">
-      <div className="pt-36 flex justify-center items-center text-black">
+      <div className="pt-16 flex justify-center items-center text-black">
         <h2 className="text-7xl">Hello {name}!</h2>
       </div>
-      <div className="flex flex-row mt-20">{children}</div>
+      <div className="flex flex-row mt-16">{children}</div>
     </div>
   );
 };
