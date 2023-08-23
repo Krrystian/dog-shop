e3d8a1b1d7a5a0740206e25b3d06ee2152b66036
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-08-24 01:30:05 +0200

    Route changes

--- app/api/category/getCategory/route.tsx
+++ app/api/category/getCategory/route.tsx
@@ -2,6 +2,6 @@ import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const products = await prisma.category.findMany();
-  return NextResponse.json(products);
+  const category = await prisma.category.findMany();
+  return NextResponse.json(category);
 }
